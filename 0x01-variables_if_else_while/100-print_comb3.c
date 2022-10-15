e8d17a3e14c5fc9ff476ba104f847ba9481614ed
jezdarmie <yestrie2@gmail.com>
2022-10-14 17:30:15 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int c, i;
 
-	for (c = '0'; c <= '9';c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		for(i = '0'; i <= '9'; i++)
 		{
-			if (c <i)
+			if (c < i)
 			{
 				putchar(c);
 				putchar(i);
