9950cc43e4d2e32655efda77baa9a5850782637d
jezdarmie <yestrie2@gmail.com>
2022-10-17 13:20:29 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ int n, m, l;
 
 for (n = 48; n < 58; n++)
 {
-	
+
 	for (m = 49; m < 58; m++)
 {
 	for (l = 50; l < 58; l++)
@@ -29,12 +29,12 @@ if (n != 55 || m != 56)
 {
 putchar(',');
 putchar(' ');
-                                      }
-                               }
+                                        }
+                                }
                         }
                 }
-         }
+        }
 putchar('\n');
 
 return (0);
-}				
+}
